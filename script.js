3474805f192124f5a35d94643de633dd04580804
arshad-hash <142026560+arshad-hash@users.noreply.github.com>
2023-10-12 18:39:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	let slot =sampleObject.keys();
+	for(let i=0 ; i<key.length; i++)
+		{
+			if(slot[i]= key)
+			{
+			 return true;
+				
+			}
+		} retun false;
+	
 }
 
 // Do not change the code below
